dde583b97763e19c2915fcbdb3bcacc1633e4ad1
Eva <evaiablocova@gmail.com>
2023-07-01 16:56:20 +0300

    DECODE

--- src/main/java/com/javarush/cryptanalyzer/iablocova/services/Decode.java
+++ src/main/java/com/javarush/cryptanalyzer/iablocova/services/Decode.java
@@ -1,19 +1,46 @@
 package com.javarush.cryptanalyzer.iablocova.services;
 
-import com.javarush.cryptanalyzer.iablocova.entity.Result;
-import com.javarush.cryptanalyzer.iablocova.repository.ResultCode;
-import com.javarush.cryptanalyzer.iablocova.exception.ApplicationException;
+import java.io.*;
 
+import com.javarush.cryptanalyzer.iablocova.constants.CryptoAlphabet;
+import com.javarush.cryptanalyzer.iablocova.entity.Result;
+import com.javarush.cryptanalyzer.iablocova.exception.ApplicationException;
+import com.javarush.cryptanalyzer.iablocova.repository.ResultCode;
+
+import static com.javarush.cryptanalyzer.iablocova.constants.ApplicationCompletionConstants.EXCEPTION;
 
 public class Decode implements Function{
     @Override
     public Result execute (String[] parameters){
-        try{
 
-            //TODO finish Decode execute method
+        try (BufferedReader reader = new BufferedReader(new FileReader(parameters[1]))) {
+            StringBuilder encryptedText = new StringBuilder();
+
+            int  keyForDecode = Integer.parseInt (parameters[2]);
+            int character;
+            while ((character = reader.read()) != -1) {
+                int currentIndex = CryptoAlphabet.ALPHABET.indexOf((char)character);
+                char encryptedChar = (char) character;
+
+                if (currentIndex != -1) {// если этот символ есть в нашем алфавите
+                    int newIndex = currentIndex - keyForDecode - 1;
+                    if (newIndex >= 0) {
+                        encryptedChar = CryptoAlphabet.ALPHABET.charAt(newIndex);
+                    } else if (newIndex < 0)
+                        encryptedChar = CryptoAlphabet.ALPHABET.charAt(newIndex + CryptoAlphabet.lengthOfAlphabet);
+                }
+
+                encryptedText.append(encryptedChar);
+            }
+//        return null;
+
+            try (FileWriter fileWriter = new FileWriter(parameters[3])) {
+                fileWriter.write(encryptedText.toString());
+            } catch (IOException e) {
+                System.out.println(EXCEPTION + e.getMessage());
+            }
         } catch (Exception e){
-            return new Result(ResultCode.ERROR, new ApplicationException("Decode operation finish with exception ", e));
+            return new Result(ResultCode.ERROR, new ApplicationException("Encode operation finish with exception ", e));
         }
-        return new Result(ResultCode.OK);
-    }
+        return new Result(ResultCode.OK);}
 }
